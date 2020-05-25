8f9455d9500758f7ad084cae22fd125ea883c708
Blanca Frade <blancafradep@gmail.com>
2020-05-25 13:59:53 +0200

    fixed group by parameters

--- Exercici Queries SQL/CONSULTES/CONSULTA3.sql
+++ Exercici Queries SQL/CONSULTES/CONSULTA3.sql
@@ -1,7 +1,4 @@
 SELECT Origin, colYear, colMonth, AVG(ArrDelay) AS prom_arribades
 FROM USAirlineFlights2.Flights
-GROUP BY flightID
-ORDER BY
-Origin,
-colYear ASC,
-colMonth ASC
+GROUP BY Origin, colYear, colMonth 
+	ORDER BY Origin, colYear, colMonth;
