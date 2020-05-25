6f3712a4f66f597fab94e9350dbbd39d6d97114b
Blanca Frade <blancafradep@gmail.com>
2020-05-25 13:58:46 +0200

    afegim el HAVING total_cancelacions > 0  per exclure resultats.

--- Exercici Queries SQL/CONSULTES/CONSULTA5.sql
+++ Exercici Queries SQL/CONSULTES/CONSULTA5.sql
@@ -1,4 +1,5 @@
-SELECT UniqueCarrier, colYear, colMonth, COUNT(Cancelled) as total_cancelled
-FROM Flights
-GROUP BY UniqueCarrier
-ORDER BY total_cancelled
+SELECT UniqueCarrier, colYear, colMonth, SUM(Cancelled) as total_cancelacions 
+FROM Flights  
+GROUP BY UniqueCarrier, colYear, colMonth  
+HAVING total_cancelacions > 0  
+ORDER BY total_cancelacions DESC, colYear ASC, colMonth ASC
